0845da4484bef98cee4ac37cc4fa4dcb89ab5ca5
jhculb <56140853+jhculb@users.noreply.github.com>
2026-01-04 13:47:11 +0000

    Deploying to gh-pages from @ jhculb/culbert.eu@b43f57bea8e52982d23cedb579c7672b053274f6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -415,24 +415,29 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+          section: "News",},{id: "projects-project-10",
+          title: 'Project 10',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-openbib",
+          title: 'OpenBib',
+          description: "Supporting Open Bibliometric Data",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-omino",
+          title: 'OMINO',
+          description: "Overcoming Multilevel Information Overload",
+          section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-the-open-research-converter",
+          title: 'The Open Research Converter',
+          description: "Expanding Reproducible Bibliometrics",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-widesky",
+          title: 'WideSky',
+          description: "Collecting and Storing the BlueSky Firehose",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
